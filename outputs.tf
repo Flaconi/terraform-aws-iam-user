34b65d494ce3e38da0bf04b5d6b4c4e37286c133
Sergey Novikov <sergey.novikov@flaconi.de>
2021-10-04 12:51:00 +0200

    Remove `this_aws_` prefix from outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
-output "this_aws_iam_user_arn" {
+output "iam_user_arn" {
   description = "The IAM User ARN"
   value       = element(concat(aws_iam_user.this.*.arn, [""]), 0)
 
 }
 
-output "this_aws_iam_user_id" {
+output "iam_user_id" {
   description = "The IAM User ID"
   value       = element(concat(aws_iam_user.this.*.id, [""]), 0)
 }
 
-output "this_aws_iam_access_key_id" {
+output "iam_access_key_id" {
   description = "The access key id"
   value       = element(concat(aws_iam_access_key.this.*.id, [""]), 0)
 }
 
-output "this_aws_iam_access_key_secret" {
+output "iam_access_key_secret" {
   description = "The access key secret"
   value       = element(concat(aws_iam_access_key.this.*.secret, [""]), 0)
   sensitive   = true
